fb0f13f3c9e3a01d3c6bca3c6dd4c57e2b3529d5
mohamad <mh711748@gmail.com>
2024-11-03 18:32:12 +0330

    API

--- src/users/Users.jsx
+++ src/users/Users.jsx
@@ -1,73 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../style.module.css'
 import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import Swal from 'sweetalert2'
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const Users = ()=>{
+const Users = () => {
     const navigate = useNavigate()
     const params = useLocation()
-    
-const handleDelete = (itemId)=>{
-    Swal.fire({
-        title: "حذف رکورد",
-        text: ` ایا از حذف رکورد ${itemId}اطمینان دارید `,
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire({
-            title: "Deleted!",
-            text: "حذف با موفقیت انجام شد",
-            icon: "success"
-          });
-        }
-      });
-} 
+    const [user, setUser] = useState([])
+
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res => {
+            setUser(res.data)
+
+        }).catch(err => {
+  
+
+        })
+    }, []);
+
+
+
+    const handleDelete = (itemId) => {
+        Swal.fire({
+            title: "حذف رکورد",
+            text: ` ایا از حذف رکورد ${itemId}اطمینان دارید `,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "حذف با موفقیت انجام شد",
+                    icon: "success"
+                });
+            }
+        });
+    }
 
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
-        <h4 className="text-center">  کاربران</h4>
-        <div className=' row my-2 mb-4 justify-content-between w-100 mx-0'>
-            <div className=' form-group col-10 col-md-6  col-lg-4 '>
-                <input type="text" className=' shadow  form-control' placeholder='جستجو' />
+            <h4 className="text-center">  کاربران</h4>
+            <div className=' row my-2 mb-4 justify-content-between w-100 mx-0'>
+                <div className=' form-group col-10 col-md-6  col-lg-4 '>
+                    <input type="text" className=' shadow  form-control' placeholder='جستجو' />
+                </div>
+                <div className=' col-2 text-start px-0'>
+                    <Link to="/User/AddUser">
+                        <button className=' btn btn-success'>
+                            <i className="fa fa-plus text-light"></i>
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <div className=' col-2 text-start px-0'>
-                <Link to="/User/AddUser">
-                <button className=' btn btn-success'>
-                    <i className="fa fa-plus text-light"></i>
-                </button>
-                </Link>
-            </div>
-        </div>
-        <table className="table bg-light shadow">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>نام</th>
-                    <th>نام کاربری</th>
-                    <th>ایمیل</th>
-                    <th>عملیات</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>mohamad</td>
-                    <td>mohamadhbi</td>
-                    <td>mh711748@gmail.com</td>
-                    <td>
-                        <i className="fas fa-edit text-warning mx-2 pointer"
-                        onClick={()=>{ return navigate("/User/AddUser/userId" , {state : "react"})}} ></i>
+            {user.length ? (
+                <table className="table bg-light shadow">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>نام</th>
+                            <th>نام کاربری</th>
+                            <th>ایمیل</th>
+                            <th>عملیات</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {user.map(u => (
+                            <tr>
+                                <td>{u.id}</td>
+                                <td>{u.name}</td>
+                                <td>{u.username}</td>
+                                <td>{u.email}</td>
+                                <td>
+                                    <i className="fas fa-edit text-warning mx-2 pointer"
+                                        onClick={() => { return navigate("/User/AddUser/userId", { state: "react" }) }} ></i>
 
-                        <i className="fas fa-trash text-danger mx-2 pointer" onClick={()=>handleDelete(1)}></i>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+                                    <i className="fas fa-trash text-danger mx-2 pointer" onClick={() => handleDelete(1)}></i>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+
+            )
+
+                :
+
+                (
+                    <h4 className=' text-center text-info'>لطفا صبر کنید</h4>
+
+                )
+
+            }
+        </div>
     )
 
 }
