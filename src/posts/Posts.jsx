c7fbc38ad5c1ce16da0126b471de5e34ef4e00a9
mohamad <mh711748@gmail.com>
2024-11-06 16:48:32 +0330

    posts-part1

--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,11 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from '../style.module.css'
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { getPostService } from '../service/PostService';
+
+
+
 
 const Posts = () => {
 
+
+
+    const [post, setPost] = useState([])
+
+
+    const navigate = useNavigate()
+
+    const handleSearch = () => {
+
+    }
+
+    const handleDelete = (postid) => {
+
+    }
+    const getposts = async () => {
+        const res = await getPostService();
+        setPost(res.data)
+    }
+
+    useEffect(() => {
+        getposts();
+    }, [])
+
+
+
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid`}>
-            <h4 className="text-center">مدیریت پست ها</h4>
+            <h4 className="text-center">  کاربران</h4>
+            <div className=' row my-2 mb-4 justify-content-between w-100 mx-0'>
+                <div className=' form-group col-10 col-md-6  col-lg-4 '>
+                    <input type="text" className=' shadow  form-control' placeholder='جستجو' onChange={handleSearch} />
+                </div>
+                <div className=' col-2 text-start px-0'>
+                    <Link to="/Posts/AddUser">
+                        <button className=' btn btn-success'>
+                            <i className="fa fa-plus text-light"></i>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+            {post.length ? (
+                <table className="table bg-light shadow">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>آی دی کاربر</th>
+                            <th>عنوان</th>
+                            <th>متن</th>
+                            <th>عملیات</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {post.map(u => (
+                            <tr key={u.id}>
+                                <td>{u.id}</td>
+                                <td>{u.userId}</td>
+                                <td>{u.title}</td>
+                                <td>{u.body}</td>
+                                <td>
+                                    <i className="fas fa-edit text-warning mx-2 pointer"
+                                        onClick={() => navigate(`/Posts/AddUser/${u.id}`)} ></i>
+
+                                    <i className="fas fa-trash text-danger mx-2 pointer" onClick={() => handleDelete(u.id)}></i>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+
+            )
+
+                :
+
+                (
+                    <>
+                        <h4 className=' text-center text-info'>لطفا صبر کنید</h4>
+                        <div className="d-flex justify-content-center">
+                            <div className="spinner-border text-info" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </div>
+                        </div>
+                    </>
+                )
+
+            }
         </div>
     )
 
---
+++ src/service/PostService.js
@@ -0,0 +1,53 @@
+import { jpAxios } from "../jpAxios";
+import Swal from "sweetalert2";
+
+
+export const getPostService = ()=>{
+    return jpAxios.get('/posts')
+}
+
+/*
+export const setUserService = async (data) => {
+    const res = await jpAxios.post(`/users`, data)
+    if (res) {
+        console.log(res);
+        Swal.fire(`${res.data.name} با موفقیت ایجاد شد`, {
+            icon: "success",
+            buttons: "متوجه شدم",
+        });
+    }
+}
+
+
+
+
+export  const updateUserService = async (data , userId) => {
+    const res = await jpAxios.put(`/users/${userId}`, data)
+    if (res) {
+        console.log(res);
+        Swal.fire(`${res.data.name} با موفقیت ویرایش شد`, {
+            icon: "success",
+            buttons: "متوجه شدم",
+        });
+    }
+}
+
+
+
+
+export const efect = (data , setData , userId)=>{
+    jpAxios.get(`/users/${userId}`,).then(res => {
+        setData({
+            name: res.data.name,
+            username: res.data.username,
+            email: res.data.email,
+            address: {
+                street: res.data.address.street,
+                city: res.data.address.city,
+                suite: res.data.address.suite,
+                zipcode: res.data.address.zipcode
+            }
+        })
+
+    });
+}*/
