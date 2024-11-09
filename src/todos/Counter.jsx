5ff69131a1bd2fdef6cd875a513739418db69748
mohamad <mh711748@gmail.com>
2024-11-09 22:33:05 +0330

    switch_useReducer

--- src/todos/Counter.jsx
+++ src/todos/Counter.jsx
@@ -4,9 +4,22 @@ const init = 0
 
 
 const reducer = (state, action) => {
-if(action == "mohamad") return state + 1
-if(action == "mohamadd") return state - 1
-if(action == 'mohamaddd') return init
+    switch (action) {
+        case "mohamad":
+            return state + 1
+
+
+        case "mohamadd":
+            return state - 1
+
+
+        case "mohamaddd":
+            return init
+
+
+        default:
+            return state
+    }
 }
 
 
@@ -18,9 +31,9 @@ const Counter = () => {
     return (
         <div className=" text-center my-3">
             <h1 className=" text-center">{count}</h1>
-            <button className=" btn btn-success" onClick={()=>Dispatch('mohamad')}>افزایش</button>
-            <button className=" btn btn-danger"onClick={()=>Dispatch('mohamadd')}>کاهش</button>
-            <button className=" btn btn-warning"onClick={()=>Dispatch('mohamaddd')}>ریست</button>
+            <button className=" btn btn-success" onClick={() => Dispatch('mohamad')}>افزایش</button>
+            <button className=" btn btn-danger" onClick={() => Dispatch('mohamadd')}>کاهش</button>
+            <button className=" btn btn-warning" onClick={() => Dispatch('mohamaddd')}>ریست</button>
         </div>
     )
 
